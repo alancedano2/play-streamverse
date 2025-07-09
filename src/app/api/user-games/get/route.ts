3ad95c455c8e76cec0612336b8ee51056fda5463
alancedano2 <supernatura94@yahoo.com>
2025-07-09 03:35:59 -0400

    Update route.ts

--- src/app/api/user-games/get/route.ts
+++ src/app/api/user-games/get/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse, NextRequest } from 'next/server';
-import { admin } from '@/lib/firebaseAdmin';
+import { db } from '@/lib/firebaseAdmin';
 
 export async function GET(request: NextRequest) {
   try {
