ae79d07eab41041626607340c4eb9c8e74532b7e
alancedano2 <supernatura94@yahoo.com>
2025-07-10 00:32:05 -0400

    Update route.ts

--- src/app/api/proxy-lanzar/route.ts
+++ src/app/api/proxy-lanzar/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: NextRequest) {
       });
     }
 
-    const res = await fetch('https://82aa21c82b66.ngrok-free.app/api/lanzar', {
+    const res = await fetch('https://85ed7ac043b9.ngrok-free.app/api/lanzar', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
